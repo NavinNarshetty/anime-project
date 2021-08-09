0e60b6220f1b9a38f9f221adac7e04ad5dc9506e
navinnarshetty <navinnarshetty@gmail.com>
2021-08-09 20:12:35 +0530

    tooltip integrated with data on card component

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,7 +1,7 @@
-import { Fragment , React} from "react";
-import {Paper , Grid, Typography , Tooltip , Button} from "@material-ui/core";
+import { Fragment , React } from "react";
+import {Paper , Grid, Typography , Tooltip , Card , CardContent } from "@material-ui/core";
 import  classes  from "./ImageCard.module.css";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles , withStyles } from "@material-ui/styles";
 
 
 const useStyles = makeStyles({
@@ -11,25 +11,50 @@ const useStyles = makeStyles({
     },
 })
 
+const HtmlTooltip = withStyles((theme) => ({
+    tooltip: {
+      backgroundColor: '#0a1625',
+      color: '#ecf6fe',
+      fontSize: 14,
+      borderRadius:15,
+    },
+}))(Tooltip);
+
+
 const ImageCard = (props) =>{
     const {response:card} = props
-    const Image = ()=>{
-    return (<img alt="animeimage" className={classes.cardResponsive} src={card.attributes.posterImage.small}></img>)
-    }
     const stlyes = useStyles()
     const onCardClicked =()=>{
        props.onClick(card.id)
     }
-    const body = <h1>Card Hover</h1>
+    const Image = ()=>{
+        return (<img alt="animeimage" className={classes.cardResponsive} src={card.attributes.posterImage.small}></img>)
+        }
+    const body = (
+        <Fragment>
+            <div className={classes.tooltipContainer}>
+                <div>
+                    <h4>
+                        {card.attributes.canonicalTitle}
+                    </h4>
+                    <p> {card.attributes.subtype} Shows: {card.attributes.episodeCount} episodes</p>
+                    <p> Status : {card.attributes.status}</p>
+                    <p>Favourite Count : {card.attributes.favoritesCount}</p>
+                    <p>Rating : {card.attributes.averageRating} %</p>
+                </div>
+            </div>
+        </Fragment>
+    )
     return (
        <Fragment>
+           
            <Grid item xs={6} sm={2}>
-              
-           <Tooltip title={body} placement="right-start" arrow>    
+           <HtmlTooltip title={body} placement="right-start" arrow >
                 <Paper className={stlyes.paper} elevation={0} onClick={onCardClicked}>
                     <Image />
                 </Paper>
-            </Tooltip>
+            </HtmlTooltip>
+           
                 <Typography component="h4">
                         {card.attributes.canonicalTitle}
                 </Typography>
